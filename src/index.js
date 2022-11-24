b173c02e29df64a67c8ec27591e7fe3e95c9131a
talles-alexandre <alexandretalles13@gmail.com>
2022-11-24 12:31:09 -0300

    rota login e cadastro feito

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+app.use(authRoutes);
 
 const PORT = process.env.PORT;
 
