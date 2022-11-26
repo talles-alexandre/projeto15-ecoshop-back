bcd018643947cf608743a3ef931f0ae9532928ba
gusgusz <gustavoguimaraescamps@gmail.com>
2022-11-26 11:54:21 -0300

    puting productsRoutes on index

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import authRoutes from "./routes/authRoutes.js";
+import productsRoutes from "./routes/productsRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(authRoutes);
+app.use(productsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
