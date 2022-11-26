07865e55f8710fb8a514d9995b9b28998045dfc8
talles-alexandre <alexandretalles13@gmail.com>
2022-11-26 12:46:04 -0300

    teste

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -1,7 +1,7 @@
 import joi from "joi";
 
 export const authCadastroSchema = joi.object({
-  name: joi.string().required(),
+  name: joi.string().min(3).required(),
   email: joi.string().email().required(),
   password: joi.string().required(),
 });
